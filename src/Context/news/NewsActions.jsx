c7eb1bfa6890271175ec609316a2ca890c167fa4
Deepeshnama <deepeshnama0786@gmail.com>
2024-06-13 00:18:04 +0530

    date change

--- src/Context/news/NewsActions.jsx
+++ src/Context/news/NewsActions.jsx
@@ -1,29 +1,27 @@
+/* eslint-disable no-unused-vars */
 
-const today = new Date(Date.now()).toLocaleDateString()
+const today = new Date(Date.now()).toLocaleDateString();
 
 // console.log(today)
 
-let year = today.split("/") [2]
-let day = today.split("/") [1]
-let month = today.split("/") [0]
+let year = today.split("/")[2];
+let day = today.split("/")[1];
+let month = today.split("/")[0];
 
 // const formattedDate = `${year}-${day}-${month}`
 
 // console.log(formattedDate)
 
-export const fetchNews = async(topic) => {
-   
-   try {
-    const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2024-${month}-0${day}&sortBy=publishedAt&apiKey=0ca33b4844b242b58ccac08b02ccbfad`)
+export const fetchNews = async (topic) => {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=${topic}&from=2024-06-13&sortBy=publishedAt&apiKey=0ca33b4844b242b58ccac08b02ccbfad`
+    );
 
-    const data = await response.json()
+    const data = await response.json();
 
     return data.articles;
-
-   } catch (error) {
-    
-    window.alert("Please Enter Valid City Name")
-
-   }
-
-}
+  } catch (error) {
+    window.alert("Please Enter Valid City Name");
+  }
+};
